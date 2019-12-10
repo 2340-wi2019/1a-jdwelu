24cd0e8017c3e7d13557d2e380d2980a819c1482
jdwelu <58574621+jdwelu@users.noreply.github.com>
2019-12-10 17:50:55 -0600

    Add files via upload

---
+++ assignment01_redux/js/myScript1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#first-name').text('James');
+    $('#last-name').text(' Welu');
+    $('.mcc').css('color', 'blue');
+    $('.mcc').css('font-style', 'italic');
+    $('#hobby').text('College');
+    $('ul').append('<li>Electronics</li>');
+    $('ul').append('<li>Films</li>');
+});
+
+// jquery(document).ready();
